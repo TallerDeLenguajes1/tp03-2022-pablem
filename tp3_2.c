3b7b881a6ed0b9990762eb11c1c1dc5736dd53e6
pablo <palmierijpablo@gmail.com>
2022-04-07 18:50:27 -0300

    primer ejercicio completo

---
+++ tp3_2.c
@@ -0,0 +1,37 @@
+/* Trabajo Práctio No3 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define N 5
+
+int main() {
+
+    char *Buff; //variable auxiliar
+    char* vector[N];
+
+    Buff = (char *) malloc(30*sizeof(char));
+
+    for (int i = 0; i < N; i++) {
+
+        printf("%d) Ingrese un nombre: ", i+1);
+        gets(Buff);
+        vector[i] = (char *) malloc((strlen(Buff)+1)*sizeof(char));
+        strcpy(vector[i], Buff);
+    }
+    free(Buff);
+    
+    printf("\n El vector de nombres es:\n");
+    printf("[ ");
+    for (int i = 0; i < N; i++) {
+        printf("%s ", vector[i]);
+    }
+    printf("]");
+
+    for (int i = 0; i < N; i++) {
+        free(vector[i]);
+    }
+    
+    return 0;
+}
